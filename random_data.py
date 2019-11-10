f9b6669c9011f92a52571f76ab413c4e11a74cc7
Fazrin Adinugraha <FazrinAdinugraha@gmail.com>
2019-11-10 09:15:34 +0700

    printing hello world

--- random_data.py
+++ random_data.py
@@ -12,6 +12,7 @@ fake_cities = ['Metropolis', 'Eerie', "King's Landing", 'Sunnydale', 'Bedrock', 
 states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
 
 for num in range(100):
+    print('hello world')
     first = random.choice(first_names)
     last = random.choice(last_names)
 
